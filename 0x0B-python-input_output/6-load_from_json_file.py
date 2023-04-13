aa39fad36b26dcbe5a308b4e82b2772f05e85035
Yinkusben <yinkusben4sure@gmail.com>
2023-04-13 19:21:13 +0000

    Fix

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" A function that creates an Object from a “JSON file """
-
+"""Defines a JSON file-reading function."""
 import json
 
+
 def load_from_json_file(filename):
-    """ Open the filw and deserialize using load() funcrion
-    """
-    with open(filename, 'r' encoding='utf-8') as f:
+    """Creates an object from a JSON file"""
+    with open(filename) as f:
         return json.load(f)

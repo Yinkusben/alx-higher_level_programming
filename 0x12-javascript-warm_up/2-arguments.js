2f775db00289d5e2b07ac2ab1d78588e1f95651c
Yinkusben <yinkusben4sure@gmail.com>
2023-06-14 08:38:22 +0000

    added file

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.lenght === 2) {
+	console.log('No arguement');
+} else if (process.argv.lenght === 3) {
+	console.log('Arguement found');
+} else {
+	console.log('Arguements found');
+}
